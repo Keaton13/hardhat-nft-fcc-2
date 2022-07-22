f153da90f46677d71761436a921cb34fc67630ae
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2022-07-22 14:23:24 -0700

    Adding chainlink vrf variables

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -16,6 +16,26 @@ contract RandomIpfsNft is VRFConsumerBaseV2 {
     // users have to pay to mint an NFT
     // the owner of the contract can withdrawl the ETH
 
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_subscriptionId = subscriptionId;
+        i_gasLane = gasLane;
+        i_callbackGasLimit = callbackGasLimit;
+        
+    }
+
     function requestNft() public {}
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
